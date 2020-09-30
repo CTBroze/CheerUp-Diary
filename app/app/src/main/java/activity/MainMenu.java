3540671bc26bac0a7fcc11c0b921b4ba4e1497d6
김건희 <38135405+Hamji@users.noreply.github.com>
2020-09-30 15:38:49 +0900

    login
    login 구현

---
+++ app/app/src/main/java/activity/MainMenu.java
@@ -0,0 +1,14 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ap.cheerupdiary.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
