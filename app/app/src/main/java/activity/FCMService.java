04133ade633f14cd3990f9d6284253a97afdee99
Minseok Kim <ctbroze@gmail.com>
2020-11-28 19:27:21 +0900

    카드메시지 출력 추가

--- app/app/src/main/java/activity/FCMService.java
+++ app/app/src/main/java/activity/FCMService.java
@@ -1,7 +1,18 @@
 package activity;
 
-import androidx.annotation.NonNull;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.ap.cheerupdiary.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -9,7 +20,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.net.URL;
+
 public class FCMService extends FirebaseMessagingService {
+    private static final String CHANNEL_ID = "0";
+
     @Override
     public void onNewToken(@NonNull String token) {
         sendToken(token);
@@ -17,9 +32,43 @@ public class FCMService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-    }
+        Bitmap bigPicture = null;
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        try {
+            URL url = new URL(remoteMessage.getData().get("image")+"=media");
+            bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("body"))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(bigPicture)
+                        .setBigContentTitle(remoteMessage.getData().get("title"))
+                        .setSummaryText(""))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 
     private void sendToken(String token){
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -29,6 +78,5 @@ public class FCMService extends FirebaseMessagingService {
         DatabaseReference mRef = database.getReference("FCM/");
 
         mRef.child(user.getUid()).setValue(token);
-
     }
 }
